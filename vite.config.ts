8d132693e3227ed8cfbb811e608860fcbaead7ce
sanderbrauwers <sanderbrauwers@gmail.com>
2025-05-22 09:53:56 +0200

    push

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // <- This is the important line
   server: {
     host: "::",
     port: 8080,
